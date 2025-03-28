34eb76034f396e1387ef89b118ba1faa03a14259
Larfi44 <yaroslav.krasulya@gmail.com>
2025-03-28 11:24:56 +0300

    adapt

--- veznan.js
+++ veznan.js
@@ -1,9 +1,8 @@
 const body = document.body;
-const theme = document.getElementById('theme');
 
 function toggleTheme() {
   body.classList.toggle('light');
   body.classList.toggle('dark');
   }
   
-  theme.addEventListener('click', toggleTheme);
+  document.getElementById('theme').addEventListener('click', toggleTheme);
